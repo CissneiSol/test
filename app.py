46ff5f8b485d73ec925e86f52db1485e5127e3a1
CissneiSol <accresalebns1@gmail.com>
2024-11-11 07:49:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 from flask_socketio import SocketIO, emit, request
 import eventlet
 import time
@@ -17,6 +18,11 @@ COOLDOWN_TIME = 0
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @socketio.on('connect')
 def handle_connect():
