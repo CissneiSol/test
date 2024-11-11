37f6a8ccbe6bb8860697646ab00f2e737e7341c9
CissneiSol <accresalebns1@gmail.com>
2024-11-11 08:30:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-# Счетчик посещений
-visits = 0
-
 @app.route('/')
-def index():
-    global visits
-    visits += 1
-    return render_template('index.html', visits=visits)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return "Hello, World!"
